83cb88c73f675440ae6ee2f90c5a7278bd24c047
Kabilan Sivanamam <kabilan.sivanamam@gmail.com>
2019-03-13 11:31:05 +0100

    update 8
    - useless services
    
    + holiday

---
+++ ferienplan/main.js
@@ -0,0 +1,10 @@
+$('document').ready(function(){
+    $('#ferien').fadeIn(300)
+
+    $('.one').fadeIn(600)
+    $('.two').fadeIn(1000)
+    $('.three').fadeIn(1400)
+    $('.four').fadeIn(1800)
+    $('.five').fadeIn(2200)
+    $('.six').fadeIn(2600)
+})
